1e8d7e6714bcf15a750c0e5e45a6c2a679b209a4
Perch <ifmay@outlook.com>
2026-06-23 01:43:30 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-    base: '/'
+export default defineConfig(({ command, mode }) => {
+    // mode 为 'production' 时使用 /RL-VIZ/，否则使用 /
+    const base = mode === 'production' ? '/RL-VIZ/' : '/'
+
+    return {
+        base: base,
+    }
 })
